24531c3cca5653f1b68c94df9d0e8b934393a720
Terence Dave Natad <terencedavenatad@gmail.com>
2024-07-16 00:07:19 +0800

    added some simple javascript

--- templates/script.js
+++ templates/script.js
@@ -1,9 +1,20 @@
-function toggleNav() {
-    var nav = document.getElementById('navLinks');
-    nav.classList.toggle('translate-x-full');
-    nav.classList.toggle('translate-x-0');
-  }
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+let scrollTimeout;
 
-  // Event listener for hamburger menu click
-  var hamburgerMenu = document.getElementById('hamburgerMenu');
-  hamburgerMenu.addEventListener('click', toggleNav);
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  clearTimeout(scrollTimeout);
+  
+ 
+  header.classList.add('hidden-header');
+  header.classList.remove('visible-header');
+  
+  scrollTimeout = setTimeout(() => {
+    header.classList.remove('hidden-header');
+    header.classList.add('visible-header');
+  }, 150);
+  
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+});
